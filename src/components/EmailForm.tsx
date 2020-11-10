6c3106dce77689df716c63b43e279b256adabc20
Deivid <deivid.cardos@gmail.com>
2020-11-10 13:11:57 -0300

    adding more files in mesage form

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -18,17 +18,17 @@ function EmailForm(){
     <div className="email-form">
       <form onSubmit={handleSubmit(onSubmit)}>
 
-        <label>Nome</label>
+      <label htmlFor="name">Nome</label>
         {errors.name && <span className="error">Campo obrigatório.</span>}
-        <input type="text" id="name" name="name" ref={register({required:true})} placeholder="Olá, como é o seu nome?"/>
+        <input type="text" id="name" name="name" ref={register({required:true})} placeholder="Olá, qual é o seu nome?"/>
 
         <label htmlFor="title">Título</label>
         {errors.title && <span className="error">Campo obrigatório.</span>}
-        <input type="text" id="title" name="title" ref={register({required:true})} placeholder="Digite um título"/>
+        <input type="text" id="title" name="title" ref={register({required:true})} placeholder="Escreve um título aí! :)"/>
 
         <label htmlFor="text">Texto</label>
         {errors.text && <span className="error">Campo obrigatório.</span>}
-        <textarea id="text" name="text" ref={register({required:true})} placeholder="O que você deseja?"/>
+        <textarea id="text" name="text" ref={register({required:true})} placeholder="Me conta uma boa novidade."/>
         
 
         <input type="submit" value="Enviar" />
